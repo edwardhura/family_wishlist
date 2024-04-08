63f195565385c964818ee2b68948b2d396da9c36
Ed <eduard.hura@gmail.com>
2024-04-08 17:45:55 +0300

    Remove old client create-react-app

--- client/src/components/index.ts
+++
@@ -1,4 +0,0 @@
-export { LoginLink } from './LoginLink'
-export { LogoutLink } from './LogoutLink'
-export { Header } from './Header'
-export { GlobalLoader } from './GlobalLoader'
--- client/src/pages/index.ts
+++
@@ -1,3 +0,0 @@
-export { Root } from './root/Page'
-export { Login } from './login/Page'
-export { Layout } from './layout/Page'
